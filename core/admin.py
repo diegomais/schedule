ff88096fde088120ee81d117879372ead347abaa
Diego Mais <diegomais.dev@gmail.com>
2021-02-15 09:38:20 -0300

    🗃 add event table

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.models import Event
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'created_at')
+    list_filter = ('user', 'date')
+
+admin.site.register(Event, EventAdmin)
